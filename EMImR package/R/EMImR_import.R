2097066d7edfcdeba5970f3bd3bc132594d73d87
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2022-06-24 13:37:30 +0100

    Add files via upload

---
+++ EMImR package/R/EMImR_import.R
@@ -0,0 +1,17 @@
+#' Import files for the EMImR function
+#'
+#' @param path file that contains data csv files
+#'
+#' @return expression, methylation and miRNA
+
+
+import_EMImR <- function(path) {
+
+  setwd(path)
+  expression <- read.csv("expression.csv")
+  expression <- rename(expression, p_value = c(2))
+  methylation <- read.csv("methylation.csv")
+  methylation  <- rename(methylation , p_value = c(3))
+  miRNA <- read.csv("miRNA.csv")
+  miRNA  <- rename(miRNA , p_value = c(3))
+}
